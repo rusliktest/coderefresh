8e682461328da8557af90050e9dcc221b173a07f
rusliktest <dshmc27@gmail.com>
2021-08-02 11:25:02 +0300

    Commit 1

---
+++ push.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process")
+
+const commit = '1'
+const branch = 'main'
+
+exec(`git add . & git commit -m "Commit ${commit}" & git push origin ${branch}`, (error, stdout, stderr) => {
+  if (error) {
+    console.log("error", error)
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`Successful commit: ${stdout}`);
+})
